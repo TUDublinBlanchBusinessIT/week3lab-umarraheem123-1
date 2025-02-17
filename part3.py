3cb45c7ecb9d86c76cd8d00c7f0ba9e2a0b7b72a
B00168191 Victor Igbinoba <B00168191@mytudublin.ie>
2025-02-17 17:52:24 +0000

    Completed part3

---
+++ part3.py
@@ -0,0 +1,12 @@
+#b00152481
+#17/02/2025
+#part3.py
+
+totalPFs = 0
+testNumber = 1
+
+while (totalPFs <= 4):
+    if PerfectNumber (totalnumber):
+        print(totalNumber, "is a perfect Number")
+        totalPFs =totalPFs +1
+        testNumber = testNumber +1
